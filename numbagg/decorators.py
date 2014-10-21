a6ab5c551c26204008a65a4fc845d0c55e02af95
Stephan Hoyer <shoyer@climate.com>
2014-10-21 13:43:46 -0700

    split decorators.py -> cache.py, tranform.py

--- numbagg/decorators.py
+++ numbagg/decorators.py
@@ -1,9 +1,10 @@
-import collections
 import inspect
-import re
 
 import numba
 import numpy as np
+
+from .cache import cached_property, FunctionCache
+from .transform import _transform_agg_source, _transform_moving_source
 
 
 def _nd_func_maker(cls, arg, *args, **kwargs):
@@ -37,37 +38,6 @@ def _validate_axis(axis, ndim):
     return axis
 
 
-class FunctionCache(dict):
-    """A simple dict-subclass for caching the return values of a function.
-    """
-    def __init__(self, func):
-        self.func = func
-
-    def __missing__(self, key):
-        value = self[key] = self.func(key)
-        return value
-
-
-class cached_property(object):
-    """A property that is only computed once per instance and then replaces
-    itself with an ordinary attribute. Deleting the attribute resets the
-    property.
-
-    Source:
-    https://github.com/pydanny/cached-property
-    https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
-    """
-    def __init__(self, func):
-        self.__doc__ = getattr(func, '__doc__')
-        self.func = func
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        value = obj.__dict__[self.func.__name__] = self.func(obj)
-        return value
-
-
 class NumbaNDReduce(object):
     def __init__(self, func, dtype_map=['float64,float64']):
         self.func = func
@@ -76,7 +46,7 @@ class NumbaNDReduce(object):
 
     @cached_property
     def transformed_func(self):
-        return _transform_agg_gufunc_source(self.func)
+        return _transform_agg_source(self.func)
 
     def _create_gufunc(self, ndim):
         # creating compiling gufunc has some significant overhead (~130ms per
@@ -124,7 +94,7 @@ class NumbaNDMoving(object):
 
     @cached_property
     def transformed_func(self):
-        return _transform_moving_gufunc_source(self.func)
+        return _transform_moving_source(self.func)
 
     @cached_property
     def gufunc(self):
@@ -144,63 +114,3 @@ class NumbaNDMoving(object):
             raise ValueError(MOVE_WINDOW_ERR_MSG % (arr.shape[axis], window))
         arr = arr.swapaxes(axis, -1)
         return self.gufunc(arr, window)
-
-
-def _apply_source_transform(func, transform_source):
-    """A horrible hack to make the syntax for writing aggregators more
-    Pythonic.
-
-    This should go away once numba is more fully featured.
-    """
-    orig_source = inspect.getsource(func)
-    source = transform_source(orig_source)
-    scope = {}
-    exec(source, globals(), scope)
-    try:
-        return scope['__transformed_func']
-    except KeyError:
-        raise TypeError('failed to rewrite function definition:\n%s'
-                        % orig_source)
-
-
-def _transform_agg_gufunc_source(func):
-    """Transforms aggregation functions into something numba can handle.
-
-    To be more precise, it converts functions with source that looks like
-
-        @ndreduce
-        def my_func(x)
-            ...
-            return foo
-
-    into
-
-        def __sub__gufunc(x, __out):
-            ...
-            __out[0] = foo
-
-    which is the form numba needs for writing a gufunc that returns a scalar
-    value.
-    """
-    def transform_source(source):
-        # nb. the right way to do this would be use Python's ast module instead
-        # of regular expressions.
-        source = re.sub(
-            r'^@ndreduce[^\n]*\ndef\s+[a-zA-Z_][a-zA-Z_0-9]*\((.*?)\)\:',
-            r'def __transformed_func(\1, __out):', source, flags=re.DOTALL)
-        source = re.sub(r'return\s+(.*)', r'__out[0] = \1', source)
-        return source
-    return _apply_source_transform(func, transform_source)
-
-
-def _transform_moving_gufunc_source(func):
-    """Transforms moving aggregation functions into something numba can handle.
-    """
-    def transform_source(source):
-        source = re.sub(
-            r'^@ndmoving[^\n]*\ndef\s+[a-zA-Z_][a-zA-Z_0-9]*\((.*?)\)\:',
-            r'def __transformed_func(\1):', source, flags=re.DOTALL)
-        source = re.sub(r'^(\s+.*)(window)', r'\1window[0]', source,
-                        flags=re.MULTILINE)
-        return source
-    return _apply_source_transform(func, transform_source)
---
+++ numbagg/transform.py
@@ -0,0 +1,65 @@
+import inspect
+import re
+
+# we need to import numpy because this module re-executes the functions
+import numpy as np
+
+
+def _apply_source_transform(func, transform_source):
+    """A horrible hack to make the syntax for writing aggregators more
+    Pythonic.
+
+    This should go away once numba is more fully featured.
+    """
+    orig_source = inspect.getsource(func)
+    source = transform_source(orig_source)
+    scope = {}
+    exec(source, globals(), scope)
+    try:
+        return scope['__transformed_func']
+    except KeyError:
+        raise TypeError('failed to rewrite function definition:\n%s'
+                        % orig_source)
+
+
+def _transform_agg_source(func):
+    """Transforms aggregation functions into something numba can handle.
+
+    To be more precise, it converts functions with source that looks like
+
+        @ndreduce
+        def my_func(x)
+            ...
+            return foo
+
+    into
+
+        def __sub__gufunc(x, __out):
+            ...
+            __out[0] = foo
+
+    which is the form numba needs for writing a gufunc that returns a scalar
+    value.
+    """
+    def transform_source(source):
+        # nb. the right way to do this would be use Python's ast module instead
+        # of regular expressions.
+        source = re.sub(
+            r'^@ndreduce[^\n]*\ndef\s+[a-zA-Z_][a-zA-Z_0-9]*\((.*?)\)\:',
+            r'def __transformed_func(\1, __out):', source, flags=re.DOTALL)
+        source = re.sub(r'return\s+(.*)', r'__out[0] = \1', source)
+        return source
+    return _apply_source_transform(func, transform_source)
+
+
+def _transform_moving_source(func):
+    """Transforms moving aggregation functions into something numba can handle.
+    """
+    def transform_source(source):
+        source = re.sub(
+            r'^@ndmoving[^\n]*\ndef\s+[a-zA-Z_][a-zA-Z_0-9]*\((.*?)\)\:',
+            r'def __transformed_func(\1):', source, flags=re.DOTALL)
+        source = re.sub(r'^(\s+.*)(window)', r'\1window[0]', source,
+                        flags=re.MULTILINE)
+        return source
+    return _apply_source_transform(func, transform_source)
