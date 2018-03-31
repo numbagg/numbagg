b9a5f2057c7ff0a1e86a5d74e7ddd53a54bb4aa6
Stephan Hoyer <shoyer@google.com>
2018-03-31 15:11:34 -0700

    Rewrite transformations to be more reliable
    - Fix Travis-CI.
    - Remove rewriting for ndmoving.
    - Use ast rather regex for rewriting ndreduce.

--- numbagg/__init__.py
+++ numbagg/__init__.py
@@ -1,5 +1,6 @@
 from .funcs import (allnan, anynan, count, nanargmax, nanargmin, nanmax,
                     nanmean, nanstd, nanvar, nanmin, nansum)
 from .moving import move_nanmean
+from .decorators import ndreduce, ndmoving
 
 dtypes = ['float32', 'float64']
