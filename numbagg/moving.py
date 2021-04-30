2e6953da0b8c6a99bd5596101352ee5d376443d0
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2021-04-30 16:34:56 -0700

    Add exp_moving_sum function (#28)
    * Add posonlyargs, required for python3.8+
    
    * Add exp_moving_sum function
    
    * Docstring
    
    * _
    
    * Simplify function, fix handling of nans
    
    * _

--- numbagg/moving.py
+++ numbagg/moving.py
@@ -6,6 +6,8 @@ from .decorators import ndmoving, ndmovingexp
 
 @ndmovingexp([(float32[:], float32, float32[:]), (float64[:], float64, float64[:])])
 def move_exp_nanmean(a, alpha, out):
+    # Inspired by pandas:
+    # https://github.com/pandas-dev/pandas/blob/1.2.x/pandas/_libs/window/aggregations.pyx#L1559.
 
     N = len(a)
     if N == 0:
@@ -35,9 +37,41 @@ def move_exp_nanmean(a, alpha, out):
                         )
                     old_wt += new_wt
         elif is_observation:
+            # The first non-nan value.
             weighted_avg = cur
 
         out[i] = weighted_avg
+
+
+@ndmovingexp([(float32[:], float32, float32[:]), (float64[:], float64, float64[:])])
+def move_exp_nansum(a, alpha, out):
+    """
+    Calculates the exponentially decayed sum.
+
+    Very similar to move_exp_nanmean, but calculates a decayed sum rather than the mean.
+    """
+
+    N = len(a)
+    if N == 0:
+        return
+
+    weight = 1.0 - alpha
+    ignore_na = False  # could add as option in the future
+    weighted_sum = 0
+
+    for i in range(N):
+        cur = a[i]
+        is_observation = not np.isnan(cur)
+        if not np.isnan(weighted_sum):
+            if is_observation or (not ignore_na):
+                weighted_sum = weight * weighted_sum
+                if is_observation:
+                    weighted_sum += cur
+        elif is_observation:
+            # The first non-nan value.
+            weighted_sum = cur
+
+        out[i] = weighted_sum
 
 
 @ndmoving(
