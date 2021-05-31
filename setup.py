df6cf044379af9e9cccaee9f501b19ff1d9ffab0
Maximilian Roos <m@maxroos.com>
2021-05-30 23:42:15 -0700

    Update setup configs

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ CLASSIFIERS = [
     "Intended Audience :: Science/Research",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "Topic :: Scientific/Engineering",
 ]
@@ -18,7 +18,7 @@ DESCRIPTION = "Fast N-dimensional aggregation functions with Numba"
 
 setup(
     name="numbagg",
-    version="0.1",
+    version="0.2.0",
     license="BSD",
     author="Stephan Hoyer",
     author_email="shoyer@gmail.com",
@@ -26,7 +26,7 @@ setup(
     description=DESCRIPTION,
     install_requires=["numpy", "numba"],
     tests_require=["pytest", "bottleneck", "pandas"],
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     url="https://github.com/numbagg/numbagg",
     test_suite="pytest",
     packages=find_packages(),
