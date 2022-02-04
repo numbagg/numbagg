19de7e8664bd72c5cb707b03a31bf40e1ffa9865
Deepak Cherian <dcherian@users.noreply.github.com>
2022-02-04 03:35:06 -0700

    Add group_nansum (#53)

--- numbagg/test/test_grouped.py
+++ numbagg/test/test_grouped.py
@@ -3,7 +3,10 @@ import pandas as pd
 import pytest
 from numpy.testing import assert_almost_equal
 
-from numbagg.grouped import group_nanmean
+from numbagg.grouped import group_nanmean, group_nansum
+
+ALL_FUNCS = [group_nanmean, group_nansum]
+NP_FUNCS = [np.mean, np.sum]
 
 
 def groupby_mean_pandas(values, group):
@@ -22,49 +25,51 @@ def test_groupby_mean_pandas():
     assert_almost_equal(expected.values, result.values)
 
 
-def test_group_nanmean_axis_1d_labels():
+@pytest.mark.parametrize("func, npfunc", zip(ALL_FUNCS, NP_FUNCS))
+def test_group_func_axis_1d_labels(func, npfunc):
     values = np.arange(5.0)
     labels = np.arange(5)
-    result = group_nanmean(values, labels)
+    result = func(values, labels)
     assert_almost_equal(values, result)
 
     values = np.arange(25.0).reshape(5, 5)
     labels = np.arange(5)
 
     with pytest.raises(ValueError) as excinfo:
-        result = group_nanmean(values, labels)
+        result = func(values, labels)
     assert "axis required" in str(excinfo.value)
 
-    result = group_nanmean(values, labels, axis=1)
+    result = func(values, labels, axis=1)
     assert_almost_equal(values, result)
 
-    result = group_nanmean(values, labels, axis=(1,))
+    result = func(values, labels, axis=(1,))
     assert_almost_equal(values, result)
 
-    result = group_nanmean(values, labels, axis=0)
+    result = func(values, labels, axis=0)
     assert_almost_equal(values.T, result)
 
     with pytest.raises(ValueError) as excinfo:
-        group_nanmean(values, labels[:4], axis=0)
+        func(values, labels[:4], axis=0)
     assert "must have same shape" in str(excinfo.value)
 
     with pytest.raises(ValueError) as excinfo:
-        group_nanmean(values, labels[:4], axis=(0,))
+        func(values, labels[:4], axis=(0,))
     assert "must have same shape" in str(excinfo.value)
 
-    result = group_nanmean(values, [0, 0, -1, 1, 1], axis=1)
+    result = func(values, [0, 0, -1, 1, 1], axis=1)
     expected = np.stack(
-        [values[:, :2].mean(axis=1), values[:, 3:].mean(axis=1)], axis=-1
+        [npfunc(values[:, :2], axis=1), npfunc(values[:, 3:], axis=1)], axis=-1
     )
     assert_almost_equal(expected, result)
 
 
-def test_group_nanmean_axis_2d_labels():
+@pytest.mark.parametrize("func", ALL_FUNCS)
+def test_group_nanmean_axis_2d_labels(func):
     values = np.arange(25.0).reshape(5, 5)
     labels = np.arange(25).reshape(5, 5)
-    result = group_nanmean(values, labels)
+    result = func(values, labels)
     assert_almost_equal(values.ravel(), result)
 
     values = np.arange(125.0).reshape(5, 5, 5)
-    result = group_nanmean(values, labels, axis=(1, 2))
+    result = func(values, labels, axis=(1, 2))
     assert_almost_equal(values.reshape(5, -1), result)
