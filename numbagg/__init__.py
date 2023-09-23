54ec9bfc68deab834164ae4c9b426d8041900426
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-09-23 00:10:26 -0700

    Replace deprecated `pkg_resources` (#103)
    * Replace deprecated `pkg_resources`
    
    Have just copied xarray's approach here

--- numbagg/__init__.py
+++ numbagg/__init__.py
@@ -1,4 +1,4 @@
-import pkg_resources
+from importlib.metadata import version as _version
 
 from .funcs import (
     allnan,
@@ -16,11 +16,11 @@ from .funcs import (
 from .moving import move_exp_nanmean, move_exp_nansum, move_mean
 
 try:
-    __version__ = pkg_resources.get_distribution("numbagg").version
+    __version__ = _version("numbagg")
 except Exception:
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.
-    __version__ = "999"
+    __version__ = "9999"
 
 __all__ = [
     allnan,
