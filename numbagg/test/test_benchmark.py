35d9edc8d916edaab40e113ebd1dc06db327c78d
Maximilian Roos <m@maxroos.com>
2023-11-20 12:53:07 -0800

    Make fixtures more concise

--- numbagg/test/test_benchmark.py
+++ numbagg/test/test_benchmark.py
@@ -1,49 +1,9 @@
 import numpy as np
 import pytest
 
-from .. import (
-    bfill,
-    ffill,
-    move_corr,
-    move_cov,
-    move_exp_nancorr,
-    move_exp_nancount,
-    move_exp_nancov,
-    move_exp_nanmean,
-    move_exp_nanstd,
-    move_exp_nansum,
-    move_exp_nanvar,
-    move_mean,
-    move_std,
-    move_sum,
-    move_var,
-    nanquantile,
-)
+from numbagg.moving import move_mean
 
-
-@pytest.fixture(
-    params=[
-        bfill,
-        ffill,
-        move_corr,
-        move_cov,
-        move_exp_nancorr,
-        move_exp_nancount,
-        move_exp_nancov,
-        move_exp_nanmean,
-        move_exp_nanstd,
-        move_exp_nansum,
-        move_exp_nanvar,
-        move_mean,
-        move_std,
-        move_sum,
-        move_var,
-        nanquantile,
-    ],
-    scope="module",
-)
-def func(request):
-    return request.param
+from .. import bfill, ffill
 
 
 @pytest.fixture(
@@ -60,15 +20,8 @@ def shape(request):
     return request.param
 
 
-# One disadvantage of having this in a fixture is that pytest keeps it around for the
-# whole test session. So we at least use module scoping so it will keep a single one for
-# all tests.
-@pytest.fixture(scope="module")
-def array(shape):
-    array = np.random.RandomState(0).rand(*shape)
-    return np.where(array > 0.1, array, np.nan)
-
-
+@pytest.mark.parametrize("func", [move_mean], indirect=True)
+@pytest.mark.parametrize("library", ["numbagg", "pandas", "bottleneck"], indirect=True)
 def test_benchmark_all(benchmark, func, func_callable, shape):
     benchmark.group = f"{func}|{shape}"
     benchmark.pedantic(
@@ -81,8 +34,7 @@ def test_benchmark_all(benchmark, func, func_callable, shape):
 
 @pytest.mark.parametrize("func", [ffill, bfill], indirect=True)
 @pytest.mark.parametrize("shape", [(10, 10, 10, 10, 1000)], indirect=True)
-@pytest.mark.parametrize("library", ["numbagg"], indirect=True)
-def test_benchmark_f_bfill(benchmark, func, func_callable, shape):
+def test_benchmark_f_bfill(benchmark, func_callable):
     """
     Was seeing some weird results for ffill and bfill — bfill was sometimes much faster
     than ffill. We can check this if we see this again.
