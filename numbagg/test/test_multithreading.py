c54a9d442161cb05ebde89749cc698561b4257d7
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2023-12-16 00:43:36 -0800

    Make warnings in CI into errors (#228)
    * Make warnings from the test suite errors
    
    (Will fail and require a fix)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- numbagg/test/test_multithreading.py
+++ numbagg/test/test_multithreading.py
@@ -1,3 +1,4 @@
+import warnings
 from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
@@ -7,11 +8,16 @@ import pytest
 @pytest.mark.parametrize("shape", [(20,)], indirect=True)
 def test_multithreading(func_callable):
     # Test whether the functions work in a multithreaded context
+    with warnings.catch_warnings():
+        # TODO: `nanquantile` raises a warning here (and not in other testes...); I
+        # can't figure out where it's coming from, and can't reproduce it locally. So
+        # I'm ignoring so that we can still raise errors on other warnings.
+        warnings.simplefilter("ignore")
 
-    # First compile outside the threadpool to ensure the caching doesn't affect it
-    func_callable()
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        foo = [executor.submit(func_callable) for _ in range(5)]
-        results = [f.result() for f in foo]
-        assert (len(results)) == 5
-        assert all([isinstance(r, np.ndarray) for r in results])
+        # First compile outside the threadpool to ensure the caching doesn't affect it
+        func_callable()
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            foo = [executor.submit(func_callable) for _ in range(5)]
+            results = [f.result() for f in foo]
+            assert (len(results)) == 5
+            assert all([isinstance(r, np.ndarray) for r in results])
