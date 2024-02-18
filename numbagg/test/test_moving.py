a67b38dbc11d257239c92e7a42878a03eafbbd63
Francesco Zanetta <zanetta.francesco@gmail.com>
2024-02-18 21:09:59 +0100

    skip too large arrays for moving aggregation tests

--- numbagg/test/test_moving.py
+++ numbagg/test/test_moving.py
@@ -71,6 +71,10 @@ def test_numerical_results_identical(func, func0):
     else:
         decimal = 5
     for i, a in enumerate(arrays(func_name)):
+
+        if a.size >= 1_000:
+            print(f"{func_name}: skipping large array with shape {a.shape}")
+            continue
         axes = range(-1, a.ndim)
         for axis in axes:
             windows = range(1, a.shape[axis])
